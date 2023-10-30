2f366c79e1ab4eaa12d1df4c891440037abab71d
Wiktor Strzelecki <strzeleckiwiktor01@gmail.com>
2023-10-30 21:58:11 +0100

    Rozszerzenie walidacji parametrów formularza (03)

--- src/main/java/com/strzelecki/loancalculator/domain/LoanCalculatorForm.java
+++ src/main/java/com/strzelecki/loancalculator/domain/LoanCalculatorForm.java
@@ -1,19 +1,23 @@
 package com.strzelecki.loancalculator.domain;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 public class LoanCalculatorForm {
 
-    @NotNull
-    @Min(1)
+    @NotNull(message = "Kwota nie może być pusta")
+    @Min(value = 1000, message = "Kwota pożyczki musi wynosić co najmniej 1000 ")
+    @Max(value = 1000000, message = "Kwota pożyczki nie może przekraczać 1000000")
     private Double amount;
 
-    @NotNull
-    @Min(1)
+    @NotNull(message = "Okres nie może być pusty")
+    @Min(value = 1, message = "Okres pożyczki musi wynosić co najmniej miesiąc")
+    @Max(value = 120, message = "Okres pożyczki nie może przekraczać 10 lat")
     private Integer term;
 
-    @NotNull
-    @Min(1)
+    @NotNull(message = "Oprocentowanie nie może być puste")
+    @Min(value = 1, message = "Oprocentowanie pożyczki musi wynosić co najmnniej 1")
+    @Max(value = 100, message = "Oprocentowanie pożyczki nie może przekraczać 100")
     private Double interest;
 
     public Double getAmount() {
